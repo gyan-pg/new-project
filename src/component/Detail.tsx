f05670f98bc46ac16300f5a4d9f60073557f60bb
takuya hatanaka <trhchop0723@gmail.com>
2022-06-25 11:36:20 +0900

    form validation added

--- src/component/Detail.tsx
+++ src/component/Detail.tsx
@@ -5,36 +5,64 @@ import { BsCalendar3 } from "react-icons/bs";
 import dayjs from "dayjs";
 import { BENCHPRESS, LEGPRESS, PULLDOWN } from "../syumokuList";
 
+type ERR = {
+  weight: string;
+  frequency: string;
+  sets: string;
+}
 
 const Detail: React.FC = () => {
+
+  const [weight, setWeight] = useState("");
+  const [frequency, setFrequency] = useState("");
+  const [sets, setSets] = useState("");
+  const [calendarDate, setCalendarDate] = useState(dayjs().format("YYYY-MM-DD"));
+  const [showCalendarFlg, setShowCalendarFlg] = useState(false);
+  const [err, setErr] = useState<ERR>({
+    weight: "",
+    frequency: "",
+    sets: ""
+  });
+  const [errFlg, setErrFlg] = useState(false);
+
   const params = useParams();
+  // アドレスからトレーニングの名前を取得する。
   const trainingName = params.query;
   let training = "";
 
   switch (trainingName) {
-      case BENCHPRESS.en:
-        training = BENCHPRESS.ja;
-        break;
-      case LEGPRESS.en:
-        training = LEGPRESS.ja;
-        break;
-      case PULLDOWN.en:
-        training = PULLDOWN.ja;
-        break;
+    case BENCHPRESS.en:
+      training = BENCHPRESS.ja;
+      break;
+    case LEGPRESS.en:
+      training = LEGPRESS.ja;
+      break;
+    case PULLDOWN.en:
+      training = PULLDOWN.ja;
+      break;
+  }
+
+  // バリデーション
+  const validNum = (num: string, type: keyof ERR) => {
+    if (num.length === 0 || num === '0') {
+      setErrFlg(true);
+      err[type] = "1以上の数値を入力してください";
+    } else if (!num.match(/^[0-9]+$/)) {
+      setErrFlg(true);
+      err[type] = "半角数字で入力してください";
+      setErr(err)
+    } else {
+      err[type] = "";
     }
+    if (err.weight === "" && err.frequency === "" && err.sets === "") {
+      setErrFlg(false);
+    }
+  }
 
-
-  const [frequency, setFrequency] = useState("");
-  const [weight, setWeight] = useState("");
-  const [sets, setSets] = useState("");
-  const [calendarDate, setCalendarDate] = useState(dayjs().format("YYYY-MM-DD"));
-  const [showCalendarFlg, setShowCalendarFlg] = useState(false);
-  // アドレスから種目名を取得する。
-  const kind = useLocation().pathname.substring(8);
-
-  console.log(kind);
-  console.log('calendarDate',calendarDate);
-  console.log(training);
+  const setValue = (setFunc:React.Dispatch<React.SetStateAction<string>>, type: keyof ERR, value:string) => {
+    validNum(value, type);
+    setFunc(value);
+  }
 
   return (
 
@@ -43,18 +71,29 @@ const Detail: React.FC = () => {
       <h2 className="text-center">{training}</h2>
       <section className="container mx-auto">
         <section className="w-1/5 mx-auto">
-          <input className="px-2 py-1 border block w-full" placeholder="ウェイト重量(kg)" type="text" inputMode="numeric" value={weight} onChange={(e: React.ChangeEvent<HTMLInputElement>) => {setWeight(e.target.value)}} />
-          <input className="px-2 py-1 border block w-full" placeholder="回数" type="text" inputMode="numeric" value={frequency} onChange={(e: React.ChangeEvent<HTMLInputElement>) => {setFrequency(e.target.value)}} />
-          <input className="px-2 py-1 border block w-full" placeholder="セット数" type="text" inputMode="numeric" value={sets} onChange={(e: React.ChangeEvent<HTMLInputElement>) => {setSets(e.target.value)}} />
-          <div className="flex border content-between items-center">
-            <span className="px-2 py-1 w-full inline-block">実施日：{calendarDate}</span>
-            <BsCalendar3 className="inline-block mr-2 hover:text-gray-500 hover:cursor-pointer"
-                         onClick={() => {setShowCalendarFlg(!showCalendarFlg)}}/>
+
+          <label className="ml-2 inline-block" htmlFor="weight">重量</label><span className="text-red-400 text-xs ml-4">{errFlg ? err.weight : ""}</span>
+          <input id="weight" className="px-2 py-1 border block w-full mb-4" placeholder="ウェイト重量(kg)" type="text" inputMode="numeric" value={weight} onChange={(e: React.ChangeEvent<HTMLInputElement>) => {setValue(setWeight,"weight",e.target.value)}} />
+          
+          <label className="ml-2 inline-block" htmlFor="frequency">回数</label><span className="text-red-400 text-xs ml-4">{errFlg ? err.frequency : ""}</span>
+          <input id="frequency" className="px-2 py-1 border block w-full mb-4" placeholder="回数" type="text" inputMode="numeric" value={frequency} onChange={(e: React.ChangeEvent<HTMLInputElement>) => {setValue(setFrequency,"frequency",e.target.value)}} />
+          
+          <label className="ml-2 inline-block" htmlFor="sets">セット数</label><span className="text-red-400 text-xs ml-4">{errFlg ? err.sets : ""}</span>
+          <input id="sets" className="px-2 py-1 border block w-full mb-4" placeholder="セット数" type="text" inputMode="numeric" value={sets} onChange={(e: React.ChangeEvent<HTMLInputElement>) => {setValue(setSets,"sets",e.target.value)}} />
+          
+          <label className="ml-2" onClick={() => {setShowCalendarFlg(!showCalendarFlg)}}>実施日</label>
+          <div className="flex border content-between items-center mb-4" onClick={() => {setShowCalendarFlg(!showCalendarFlg)}}>
+            <span className="px-2 py-1 w-full inline-block">{calendarDate}</span>
+            <BsCalendar3 className="inline-block mr-2 hover:text-gray-500 hover:cursor-pointer transition transition-duration-500"/>
           </div>
+
         </section>
         <section className="w-2/5 text-center mx-auto">
           {showCalendarFlg ? <Calendar today={calendarDate} setDay={setCalendarDate}/> : ""}
         </section>
+        <div className="text-center">
+          <button className={`border inline-block w-1/5 bg-blue-200 py-1 ${errFlg ? "bg-gray-200" : ""}`}>登録</button>
+        </div>
       </section>
     </>
   );
