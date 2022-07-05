e4bc7cb964cfdac47b3fdcd8e251b95f7b9f8997
takuya hatanaka <trhchop0723@gmail.com>
2022-07-05 23:54:33 +0900

    グラフの表示期間の変更機能の雛形を実装。要修正。

--- src/component/Chart.tsx
+++ src/component/Chart.tsx
@@ -14,6 +14,7 @@ import {
 
 import { Line } from 'react-chartjs-2';
 import Comment from './Comment';
+import { isPropertyAccessOrQualifiedName } from 'typescript';
 
 ChartJS.register(
   CategoryScale,
@@ -36,12 +37,24 @@ type ChartData = {
 const Chart: React.FC<any> = ({trainingData, trainingTitle}) => {
   console.log('chart render');
 
+  // TODO:リファクタリング
   const [displayWidth, setDisplayWidth] = useState(0);
   const [commentFlg, setCommentflg] = useState(false);
+  // 注釈のモーションの制御
   const [animateFlg, setAnimateflg] = useState(false);
+  // chart.jsに渡すデータ
   const [visibleData, setVisibleData] = useState([]);
   const [visibleLabel, setVisibleLabel] = useState([]);
-  
+  // 全期間の総負荷データ
+  const [processedData, setProcessedData] = useState([]);
+  // 全期間のラベル
+  const [processedLabel, setProcessedLabel] = useState([]);
+
+  const [recordNum, setRecordNum] = useState(0);
+  const [recordPages, setRecordPages] = useState(0);
+  const [totalPage, setTotalPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(0);
+
   const toggleShow = () => {
     setAnimateflg(!animateFlg);
     
@@ -64,12 +77,18 @@ const Chart: React.FC<any> = ({trainingData, trainingTitle}) => {
     const amount = data.map((elm:ChartData) => {
       return Number(elm.weight) * Number(elm.sets) * Number(elm.frequency);
     });
+    // レコードの制御に必要な数値をセット
+    const numOfPages = Math.ceil(trainingData.length / 10);
+    setRecordNum(trainingData.length);
+    setRecordPages(numOfPages);
+    setTotalPage(numOfPages);
+    setProcessedData(amount);
+    setCurrentPage(numOfPages);
+    setProcessedLabel(labelArr);
 
-    console.log(amount);
     const setData = () => {
       if(trainingData.length > 10) {
         const tempData = amount.slice(trainingData.length-10);
-        console.log(tempData);
         const tempLabel = labelArr.slice(trainingData.length-10);
         const returnData = {data: tempData, label: tempLabel};
         return returnData;
@@ -83,17 +102,35 @@ const Chart: React.FC<any> = ({trainingData, trainingTitle}) => {
 
     const tmp:any = setData();
     console.log("tmp",tmp);
-    setDisplayWidth(trainingData.length * 30);
+    setDisplayWidth(trainingData.length < 10 ? trainingData.length * 40 : 400);
     setVisibleData(tmp.data);
     setVisibleLabel(tmp.label);
 
   },[trainingData])
 
-  
+  const prevPage = () => {
+    const newPage = currentPage - 1;
+    setCurrentPage(newPage);
+    if (newPage === 1) {
+      setVisibleData(processedData.slice(0, 10));
+      setVisibleLabel(processedLabel.slice(0, 10));
+    } else if(newPage > 1 && newPage < totalPage){
+      setVisibleData(processedData.slice(recordNum - ((totalPage - newPage) * 10 - 1), recordNum - ((totalPage - (newPage + 1)))));
+      setVisibleLabel(processedLabel.slice(recordNum - ((totalPage - newPage) * 10 - 1), recordNum - ((totalPage - (newPage + 1)))))
+    }
+  }
 
-  // console.log("sorted",data);
-
-  // console.log(labelArr)
+  const nextPage = () => {
+    const newPage = currentPage + 1;
+    setCurrentPage(newPage);
+    if (newPage === totalPage) {
+      setVisibleData(processedData.slice(0, 10));
+      setVisibleLabel(processedLabel.slice(0, 10));
+    } else {
+      setVisibleData(processedData.slice((newPage - 1) * 10, newPage * 10 + 1));
+      setVisibleLabel(processedLabel.slice((newPage - 1) * 10, newPage * 10 + 1));
+    }
+  }
   
   const graphData = {
     labels: visibleLabel,
@@ -123,7 +160,7 @@ const Chart: React.FC<any> = ({trainingData, trainingTitle}) => {
             <>
               <h2 className="text-center">{trainingTitle}:総負荷グラフ<AiOutlineQuestionCircle className="inline-block hover:cursor-pointer" style={{ position: "relative", top: "-2px" }} onClick={() => toggleShow()}/></h2>
               <div className="flex justify-center">
-                <div className="pb-20 relative" style={{ height: "400px", width: `${displayWidth < 150 ? 150 : displayWidth}px` }}>
+                <div className="pb-10 relative" style={{ height: "400px", width: `${displayWidth < 200 ? 200 : displayWidth}px` }}>
                   <div className="flex items-center justify-center">
                     {commentFlg ? <Comment animate={animateFlg}/> : ""}
                   </div>
@@ -138,6 +175,16 @@ const Chart: React.FC<any> = ({trainingData, trainingTitle}) => {
           :
             "データが登録されていません。"
         }
+         
+          <div className="pb-10 flex justify-center">
+          {totalPage > 1 ?
+            <> 
+              {currentPage === 1 ? "" : <button className="px-3 py-1" onClick={() => prevPage()}>前の期間</button>}
+              {totalPage > currentPage ? <button className="px-3 py-1" onClick={() => nextPage()}>次の期間</button> : ""}
+            </>
+          : ""}
+          </div>
+        
       </section>
     </>
   );
