7a4bf6d25a1712b9e2aaa601feecb1b33e3e4528
takuya hatanaka <trhchop0723@gmail.com>
2022-07-28 07:01:51 +0900

    topページデザイン修正

--- src/component/Top.tsx
+++ src/component/Top.tsx
@@ -28,7 +28,7 @@ const Top = () => {
   const section1Offset = 1;
   const section2Offset = section1Offset + 1;
   const section3Offset = section2Offset + 2.5;
-  const section4Offset = section3Offset + 2;
+  const section4Offset = section3Offset + 3;
   const section5Offset = section4Offset + 1;
   const totalPage = section5Offset + 1;
 
@@ -38,35 +38,43 @@ const Top = () => {
       // 100vh = 1として換算
       let scrollHeight = parallax.current.current / window.innerHeight;
 
-      if (scrollHeight > 4.6) {
+      if (scrollHeight > 4.6) document.querySelector('.js-slide-in')?.classList.add('active');
+      if (scrollHeight > 5.4 || scrollHeight < 4.6)
+        document.querySelector('.js-slide-in')?.classList.remove('active');
+      if (scrollHeight > 5.6) {
         document.querySelector('.js-slide-in1')?.classList.add('active');
       } else {
         document.querySelector('.js-slide-in1')?.classList.remove('active');
       }
-      if (scrollHeight > 4.7) {
+      if (scrollHeight > 5.7) {
         document.querySelector('.js-slide-in2')?.classList.add('active');
       } else {
         document.querySelector('.js-slide-in2')?.classList.remove('active');
       }
-      if (scrollHeight > 4.8) {
+      if (scrollHeight > 5.8) {
         document.querySelector('.js-slide-in3')?.classList.add('active');
       } else {
         document.querySelector('.js-slide-in3')?.classList.remove('active');
       }
-      if (scrollHeight > 4.9) {
+      if (scrollHeight > 5.9) {
         document.querySelector('.js-slide-in4')?.classList.add('active');
       } else {
         document.querySelector('.js-slide-in4')?.classList.remove('active');
       }
-      if (scrollHeight > 5) {
+      if (scrollHeight > 6) {
         document.querySelector('.js-slide-in5')?.classList.add('active');
       } else {
         document.querySelector('.js-slide-in5')?.classList.remove('active');
       }
-      if (scrollHeight > 5.2) {
+      if (scrollHeight > 6.2) {
         document.querySelector('.js-slide-in6')?.classList.add('active');
       } else {
         document.querySelector('.js-slide-in6')?.classList.remove('active');
+      }
+      if (scrollHeight > 7) {
+        document.querySelector('.js-slide-in7')?.classList.add('active');
+      } else {
+        document.querySelector('.js-slide-in7')?.classList.remove('active');
       }
     });
   });
@@ -90,7 +98,9 @@ const Top = () => {
         {/* <section id="top" className="h-screen flex justify-center items-center"> */}
         <div className={`relative font-mono h-screen w-screen flex items-center justify-center`}>
           <div>
-            <p className="text-6xl text-center">Strength training won't betray you!!!!</p>
+            <p className="text-6xl text-center headMessage">
+              Strength training won't betray you!!!!
+            </p>
             <p className="mt-5 font-sans text-center">
               筋トレはあなたを裏切らない！！<span className="text-gray-300 text-xs">たぶん</span>
             </p>
@@ -156,41 +166,73 @@ const Top = () => {
         {/* <section id="benefit" className="h-screen"> */}
         <ParallaxLayer
           offset={section3Offset}
-          sticky={{ start: section3Offset, end: section3Offset + 1 }}
+          sticky={{ start: section3Offset, end: section3Offset + 2 }}
         >
           <h2 className={styles.sectionTitle}>筋トレの効能</h2>
           <div className={styles.sectionContent}>
-            <div className={styles.sectionInner}>
-              <h3 className="js-slide-in1 benefit offset1">痩せやすい体になる！</h3>
-              <h3 className="js-slide-in2 benefit offset2">自律神経が整う＝快眠効果！</h3>
-              <h3 className="js-slide-in3 benefit offset3">記憶力がアップする！</h3>
-              <h3 className="js-slide-in4 benefit offset4">若々しさを保てる！</h3>
-              <h3 className="js-slide-in5 benefit offset5">ストレス発散！</h3>
+            <div className={styles.sectionInner} id="benefitContainer">
+              <div className="js-slide-in introduce">素晴らしき筋トレの効果をご紹介します！</div>
+              <h3 className="js-slide-in1 benefit offset1">
+                <p className="benefitInner">
+                  <span className="benefitNo">1.</span>痩せやすい体になる！
+                </p>
+              </h3>
+              <h3 className="js-slide-in2 benefit offset2">
+                <p className="benefitInner">
+                  <span className="benefitNo">2.</span>自律神経が整う＝快眠効果！
+                </p>
+              </h3>
+
+              <h3 className="js-slide-in3 benefit offset3">
+                <p className="benefitInner">
+                  <span className="benefitNo">3.</span>記憶力がアップする！
+                </p>
+              </h3>
+              <h3 className="js-slide-in4 benefit offset4">
+                <p className="benefitInner">
+                  <span className="benefitNo">4.</span>若々しさを保てる！
+                </p>
+              </h3>
+              <h3 className="js-slide-in5 benefit offset5">
+                <p className="benefitInner">
+                  <span className="benefitNo">5.</span>ストレス発散！
+                </p>
+              </h3>
             </div>
-            <p className="js-slide-in6 benefit">などなど、たくさんの効能が期待できます！</p>
+            <p className="js-slide-in6 benefit">などなど、良い効能が期待できます！</p>
           </div>
         </ParallaxLayer>
         {/* </section> */}
         {/* <section id="recommend" className="h-screen"> */}
         <ParallaxLayer offset={section4Offset}>
-          <h2>筋トレにおいて必要なもの</h2>
-          <p>
-            ここまで、筋トレの良い側面についてお伝えしましたが、なぜみなさん筋トレを日常的に行わないのでしょうか？
-          </p>
-          <p>答えは、辛いからです！筋トレをするのには、多少の苦痛が伴います。</p>
-          <p>
-            この辛さから逃れる術は残念ながらありません。むしろ辛さを楽しむ心の余裕を持ちましょう。
-          </p>
-          <p>
-            心の余裕の他に、もう一つ必要なものがあります。それは、日々の筋トレの記録を取ることです。
-          </p>
-          <p>筋肉を成長させるには、常に今の自分の限界に対し適切な負荷をかける必要があります。</p>
-          <p>1kgの重りが楽に感じたら、次は1.5kgの重りを使うといった具合です。</p>
-          <p>何事もそうですが、楽をしていても成長はしないのです。</p>
-          <p>
-            記録をつけていくことで自身の成長を可視化でき、更なる筋トレへのモチベーションへとつながることでしょう。
-          </p>
-          <p>このアプリでは、筋トレの記録について皆さんのお手伝いが可能です。</p>
+          <div className="doubleColumnContainer">
+            <div className="doubleColumnLeft js-slide-in7">
+              <h2 className="doubleColumnCenter">筋トレにおいて必要なもの</h2>
+            </div>
+            <div className="doubleColumnRight">
+              <div className="doubleColumnCenter">
+                <p>
+                  ここまで筋トレの良い側面についてお伝えしましたが、なぜ筋トレを日常的に行わない方が多いのでしょうか？
+                </p>
+                <p>答えは、辛いからです！筋トレをするのには、多少の苦痛が伴います。</p>
+                <p>
+                  この辛さから逃れる術は残念ながらありません。むしろ辛さを楽しむ心の余裕を持ちましょう。
+                </p>
+                <p>
+                  心の余裕の他に、もう一つ必要なものがあります。それは、日々の筋トレの記録を取ることです。
+                </p>
+                <p>
+                  筋肉を成長させるには、常に今の自分の限界に対し適切な負荷をかける必要があります。
+                </p>
+                <p>1kgの重りが楽に感じたら、次は1.5kgの重りを使うといった具合です。</p>
+                <p>何事もそうですが、楽をしていても成長はしないのです。</p>
+                <p>
+                  記録をつけていくことで自身の成長を可視化でき、更なる筋トレへのモチベーションへとつながることでしょう。
+                </p>
+                <p>このアプリでは、筋トレの記録について皆さんのお手伝いが可能です。</p>
+              </div>
+            </div>
+          </div>
         </ParallaxLayer>
         {/* </section> */}
         {/* <section id="conclusion" className="h-screen"> */}
